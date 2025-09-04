a12170aa8162ef45ac1625fb509214453855b1fc
patilpradnesh <patilpradnesh4124@gmail.com>
2025-09-04 17:54:04 +0530

    adding typewriter effect

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Globe from "../component/Globe";
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
 export const Home = () => {
   const globeRef = useRef();
@@ -14,49 +15,92 @@ export const Home = () => {
   }, []);
 
   return (
-    <div style={{ position: "relative", width: "100vw", height: "100vh", overflow: "hidden" }}>
-     
-      <div style={{
-        position: "absolute",
-        top: 0, left: 0, width: "100vw", height: "100vh", zIndex: 0,
-        pointerEvents: "none", display: "flex", alignItems: "center", justifyContent: "center"
-      }}>
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          zIndex: 0,
+          pointerEvents: "none",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Globe ref={globeRef} />
       </div>
 
-    
-      <div style={{
-        position: "absolute",
-        top: "50%",
-        left: "5%",
-        transform: "translateY(-50%)",
-        zIndex: 1,
-        width: "300px"
-      }}>
-        <div className=" bg-opacity-75 p-4 rounded shadow text-light">
-          <h2>Welcome!</h2>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "5%",
+          transform: "translateY(-50%)",
+          zIndex: 1,
+          width: "300px",
+        }}
+      >
+        <div className=" bg-opacity-75 p-4 rounded shadow text-light ">
+          <h2>
+            <Typewriter
+              options={{
+                strings: [
+                  "Welcome to Earthquake Visualizer!",
+                  "Explore recent earthquake activity.",
+                  "Visualize seismic events on an interactive globe.",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h2>
+
           <p>
-            Explore recent earthquake activity around the world.<br />
-            Visualize seismic events on an interactive globe.
+            Discover earthquake patterns across the world
+            <br />
+            Your window into Earth's movements.
           </p>
         </div>
       </div>
 
-      
-      <div style={{
-        position: "absolute",
-        top: "50%",
-        right: "5%",
-        transform: "translateY(-50%)",
-        zIndex: 1,
-        width: "300px"
-      }}>
-        <div className=" bg-opacity-75 p-4 rounded shadow text-light">
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          right: "5%",
+          transform: "translateY(-50%)",
+          zIndex: 1,
+          width: "300px",
+        }}
+      >
+        <div className=" bg-opacity-75 p-4 rounded shadow text-light ">
           <h2>Navigate Towards Information</h2>
           <ul className="list-unstyled">
-            <li><Link to="/about" className="btn btn-outline-info btn-block mb-2">About</Link></li>
-            <li><Link to="/map" className="btn btn-outline-info btn-block mb-2">View Map</Link></li>
-            <li><Link to="/data" className="btn btn-outline-info btn-block mb-2">Earthquake List</Link></li>
+            <li>
+              <Link to="/about" className="btn btn-outline-info btn-block mb-2">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/map" className="btn btn-outline-info btn-block mb-2">
+                View Map
+              </Link>
+            </li>
+            <li>
+              <Link to="/data" className="btn btn-outline-info btn-block mb-2">
+                Earthquake List
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
